be6118b9801476306e41d0cc9fa13483233be21d
Aditya Singh <1215adityasingh1215@gmail.com>
2023-05-26 14:39:00 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This product is being developed to serve users one stop solution for all their NLP needs',
+    author='DS_NLP_CORE',
+    license='MIT',
+)
